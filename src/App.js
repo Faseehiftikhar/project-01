b88619fd35e15adbdd335eb0bf73ada897b354ed
faseehiftikhar <ahmadfaseeh31@gmail.com>
2020-06-09 17:29:09 +0530

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hi from Faseeh</code> 
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="Mybio">
+    <div className="Name">Hello World From <strong className="FullName">Faseeh Iftkhar</strong></div>
+
+  <div><h1>My short Introduction</h1>
+
+  <div className="ShortIntro">Full Name = Faseeh iftikhar
+  <br/>
+  
+  Age = 20 years
+  <br/>
+  
+  Education = Undergraduate</div></div>
+
+  <div><h1>My Favourite Hobbies</h1>
+  <ol>
+    <li className="OrderList">Playing Cricket</li>
+    <li>Reading Books</li>
+    <li>Watching Movies</li>
+  </ol></div>
+
+  <div><h1>My Favourite Countries</h1>
+
+  <ul>
+    <li>Pakistan</li>
+    <li>Turkey</li>
+    <li>Italy</li>
+  </ul>
+  
+</div>
+
+<div><h1>My Age In Calculation</h1> <strong>{10+10}</strong></div>
+  
+
+
+  </div>
+    
 }
 
 export default App;
